c97465008ffc376941345da1917753197c9d4d23
noahlattari <zennoah1998@gmail.com>
2019-02-23 13:12:36 -0500

    added backend/routing

---
+++ db.py
@@ -0,0 +1,30 @@
+import sqlite3
+from os import path, getcwd
+
+db = path.join(getcwd(), 'database.db')
+
+
+class Database:
+
+    def __init__(self):
+        self.connection = sqlite3.connect('database.db', check_same_thread=False)
+
+    def query(self, q, arg=()):
+        cursor = self.connection.cursor()
+
+        cursor.execute(q, arg)
+        results = cursor.fetchall()
+        cursor.close()
+
+        return results
+
+    def insert(self, q, arg=()):
+
+        cursor = self.connection.cursor()
+
+        cursor.execute(q, arg)
+
+        self.connection.commit()
+        result = cursor.lastrowid
+        cursor.close()
+        return result
