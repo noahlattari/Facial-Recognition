ddf8ed047951582d4c6bae2ed09415a5e00b174c
noahlattari <zennoah1998@gmail.com>
2019-02-23 16:26:15 -0500

    network can recognize images

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from werkzeug.utils import secure_filename
 from os import path, getcwd
 import time
 from db import Database
+from face import Face
+
 
 app = Flask(__name__)
 
 app.config['file_allowed'] = ['image/png', 'image/jpeg']
 app.config['storage'] = path.join(getcwd(), 'storage')
 app.db = Database()
-
+app.face = Face(app)
 
 def success_handle(output, status=200, mimetype='application/json'):
     return Response(output, status=status, mimetype=mimetype)
@@ -18,6 +20,40 @@ def success_handle(output, status=200, mimetype='application/json'):
 def error_handle(error_message, status=500, mimetype='application/json'):
     return Response(json.dumps({"error": {"message": error_message}}), status=status, mimetype=mimetype)
 
+def get_user_by_id(user_id):
+    user = {}
+    results = app.db.select(
+        'SELECT users.id, users.name, users.created, faces.id, faces.user_id, faces.filename,faces.created FROM users LEFT JOIN faces ON faces.user_id = users.id WHERE users.id = ?',
+        [user_id])
+
+    index = 0
+    for row in results:
+        print(row)
+        face = {
+            "id": row[3],
+            "user_id": row[4],
+            "filename": row[5],
+            "created": row[6],
+        }
+        if index == 0:
+            user = {
+                "id": row[0],
+                "name": row[1],
+                "created": row[2],
+                "faces": [],
+            }
+        if 3 in row:
+            user["faces"].append(face)
+        index = index + 1
+
+    if 'id' in user:
+        return user
+    return None
+
+def delete_user_by_id(user_id):
+    app.db.delete('DELETE FROM users WHERE users.id = ?', [user_id])
+    #also delete all faces with user id
+    app.db.delete('DELETE FROM faces WHERE faces.user_id = ?', [user_id])
 
 @app.route('/', methods=['GET'])
 def homepage():
@@ -52,8 +88,8 @@ def train():
 
             print("File is allowed and will be saved in ", app.config['storage'])
             filename = secure_filename(file.filename)
-
-            file.save(path.join(app.config['storage'], filename))
+            trained_storage = path.join(app.config['storage'], 'trained')
+            file.save(path.join(trained_storage, filename))
             # let start save file to our storage
 
             # save to our sqlite database.db
@@ -87,6 +123,44 @@ def train():
         print("Request is contain image")
     return success_handle(output)
 #route for user profile
-@app.route('/apiusers')
+@app.route('/api/users/<int:user_id>', methods=['GET', 'DELETE'])
+def user_profile(user_id):
+
+    if request.method == 'GET':
+        user = get_user_by_id(user_id)
+        if user:
+            return success_handle(json.dumps(user), 200)
+        else:
+            return error_handle("User not found", 404)
+    if request.method == 'DELETE':
+        delete_user_by_id(user_id)
+        return success_handle(json.dumps({"deleted": True}))
+
+#router for recognizing unknown face
+@app.route('/api/recognize', methods=['POST'])
+def recognize():
+    if 'file' not in request.files:
+        return error_handle("Image is required")
+    else:
+        file = request.files['file']
+        #file extension validate
+        if file.mimetype not in app.config['file_allowed']:
+            return error_handle("File extension is not allowed")
+        else:
+            filename = secure_filename(file.filename)
+            unknown_store = path.join(app.config["storage"], 'unknown')
+            file_Path = path.join(unknown_store, filename)
+            file.save(file_Path)
+
+            user_id = app.face.recognize(filename)
+
+            if user_id:
+                user = get_user_by_id(user_id)
+                message = {"message": "hey we found {0} matched with your face image".format(user["name"]), "user": user}
+                return success_handle(json.dumps(message))
+            else:
+                return error_handle("Sorry we can't find any matches with your face")
+
+    return success_handle(json.dumps({"filename_to_compare_is": filename}))
 
 app.run()
